1c8c0b6377d93b4e08e0ec6c0a74f191dd067255
Azzam <48726420+omxqn@users.noreply.github.com>
2023-05-21 16:41:44 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,140 +1,141 @@
-import evdev
-import time
-from database import *
-from servo import main_f,turn_on_red,turn_off_red
-import threading
-
-def antina():
-    while True:
-        # Find the input device with the name "RFID reader"
-        devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
-        rfid_device = None
-
-
-        for device in devices:
-
-            if device.name == 'BARCODE SCANNER Keyboard Interface':
-                rfid_device = device
-                break
-        global data
-        data = []
+while True:
+    try:
+        import evdev
+        import time
+        from database import *
+        from servo import main_f,turn_on_red,turn_off_red
+        import threading
+        print("Imported successfuly")
+        def antina():
+            while True:
+                # Find the input device with the name "RFID reader"
+                devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+                rfid_device = None
 
 
-        # If the RFID reader was found, read from it
-        if rfid_device is not None:
-            if data == []:
-                print('Found RFID reader:', rfid_device.name)
-            global rfid_tag
-            rfid_tag = ''
-            for event in rfid_device.read_loop():
-                if event.type == evdev.ecodes.EV_KEY and event.value == 1:
-                    digit = evdev.categorize(event).keycode
-                    if digit == "KEY_ENTER":
-                        rfid_tag = str(rfid_tag)
-                        datas = search_user(rfid_tag)
+                for device in devices:
 
-                        if datas == False:
-                            print(f'RFID tag ({rfid_tag}) has been read.')
-                            print("You don't have authorization ")
-                            log_history(f"card: {rfid_tag}", "Authorization Error")
-                            rfid_tag = ""
-                            time.sleep(1)
-                            turn_on_red()
-                            time.sleep(2)
-                            turn_off_red()
-                            break
+                    if device.name == 'BARCODE SCANNER Keyboard Interface':
+                        rfid_device = device
+                        break
+                global data
+                data = []
 
-                        else:
 
-                            print(
-                                f"\nCard number: {rfid_tag}({datas[0]}) with Owner_name: {datas[1]}({datas[2]}), Rank:{datas[4]}-{datas[5]} and phone number: {datas[3]}. Has entered\n")
-                            log_history(datas[2],
-                                        f"Card number: {rfid_tag}({datas[0]}) with Owner_name: {datas[1]}({datas[2]}), Rank:{datas[4]}-{datas[5]} and phone number: {datas[3]}. Has entered")
-                            rfid_tag = ""
-                            if datas[0] =="card":
-                                print(datas[0] )
-                                main_f(card=True)
+                # If the RFID reader was found, read from it
+                if rfid_device is not None:
+                    if data == []:
+                        print('Found RFID reader:', rfid_device.name)
+                    global rfid_tag
+                    rfid_tag = ''
+                    for event in rfid_device.read_loop():
+                        if event.type == evdev.ecodes.EV_KEY and event.value == 1:
+                            digit = evdev.categorize(event).keycode
+                            if digit == "KEY_ENTER":
+                                rfid_tag = str(rfid_tag)
+                                datas = search_user(rfid_tag)
+
+                                if datas == False:
+                                    print(f'RFID tag ({rfid_tag}) has been read.')
+                                    print("You don't have authorization ")
+                                    log_history(f"card: {rfid_tag}", "Authorization Error")
+                                    rfid_tag = ""
+                                    time.sleep(1)
+                                    turn_on_red()
+                                    time.sleep(2)
+                                    turn_off_red()
+                                    break
+
+                                else:
+
+                                    print(
+                                        f"\nCard number: {rfid_tag}({datas[0]}) with Owner_name: {datas[1]}({datas[2]}), Rank:{datas[4]}-{datas[5]} and phone number: {datas[3]}. Has entered\n")
+                                    log_history(datas[2],
+                                                f"Card number: {rfid_tag}({datas[0]}) with Owner_name: {datas[1]}({datas[2]}), Rank:{datas[4]}-{datas[5]} and phone number: {datas[3]}. Has entered")
+                                    rfid_tag = ""
+                                    main_f(card=False)
+                                    break
                             else:
-                                main_f(card=False)
 
-                            break
+                                if digit[-1].isdigit():
+                                    rfid_tag += digit[-1]
+                else:
+                    print('RFID reader not found.')
 
-                    else:
-
-                        if digit[-1].isdigit():
-                            rfid_tag += digit[-1]
-        else:
-            print('RFID reader not found.')
-
-            time.sleep(5)
+                    time.sleep(5)
 
 
 
-def card_reader():
-    while True:
-        # Find the input device with the name "RFID reader"
-        devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
-        rfid_device = None
+        def card_reader():
+            while True:
+                # Find the input device with the name "RFID reader"
+                devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+                rfid_device = None
 
 
-        for device in devices:
+                for device in devices:
 
-            if device.name == 'Sycreader USB Reader':
-                rfid_device = device
-                break
-        global data
-        data = []
+                    if device.name == 'Sycreader USB Reader':
+                        rfid_device = device
+                        break
+                global data
+                data = []
 
 
-        # If the RFID reader was found, read from it
-        if rfid_device is not None:
-            if data == []:
-                print('Found RFID reader:', rfid_device.name)
-            global rfid_tag
-            rfid_tag = ''
-            for event in rfid_device.read_loop():
-                if event.type == evdev.ecodes.EV_KEY and event.value == 1:
-                    digit = evdev.categorize(event).keycode
-                    if digit == "KEY_ENTER":
-                        rfid_tag = str(rfid_tag)
-                        datas = search_user(rfid_tag)
+                # If the RFID reader was found, read from it
+                if rfid_device is not None:
+                    if data == []:
+                        print('Found RFID reader:', rfid_device.name)
+                    global rfid_tag
+                    rfid_tag = ''
+                    for event in rfid_device.read_loop():
+                        if event.type == evdev.ecodes.EV_KEY and event.value == 1:
+                            digit = evdev.categorize(event).keycode
+                            if digit == "KEY_ENTER":
+                                rfid_tag = str(rfid_tag)
+                                datas = search_user(rfid_tag)
 
-                        if datas == False:
-                            print(f'RFID tag ({rfid_tag}) has been read.')
-                            print("You don't have authorization ")
-                            log_history(f"card: {rfid_tag}", "Authorization Error")
-                            rfid_tag = ""
-                            time.sleep(1)
-                            turn_on_red()
-                            time.sleep(2)
-                            turn_off_red()
-                            break
+                                if datas == False:
+                                    print(f'RFID tag ({rfid_tag}) has been read.')
+                                    print("You don't have authorization ")
+                                    log_history(f"card: {rfid_tag}", "Authorization Error")
+                                    rfid_tag = ""
+                                    time.sleep(1)
+                                    turn_on_red()
+                                    time.sleep(2)
+                                    turn_off_red()
+                                    break
 
-                        else:
+                                else:
 
-                            print(
-                                f"\nCard number: {rfid_tag}({datas[0]}) with Owner_name: {datas[1]}({datas[2]}), Rank:{datas[4]}-{datas[5]} and phone number: {datas[3]}. Has entered\n")
-                            log_history(datas[2],
-                                        f"Card number: {rfid_tag}({datas[0]}) with Owner_name: {datas[1]}({datas[2]}), Rank:{datas[4]}-{datas[5]} and phone number: {datas[3]}. Has entered")
-                            rfid_tag = ""
-                            if datas[0] =="card":
-                                print(datas[0] )
-                                main_f(card=True)
+                                    print(
+                                        f"\nCard number: {rfid_tag}({datas[0]}) with Owner_name: {datas[1]}({datas[2]}), Rank:{datas[4]}-{datas[5]} and phone number: {datas[3]}. Has entered\n")
+                                    log_history(datas[2],
+                                                f"Card number: {rfid_tag}({datas[0]}) with Owner_name: {datas[1]}({datas[2]}), Rank:{datas[4]}-{datas[5]} and phone number: {datas[3]}. Has entered")
+                                    rfid_tag = ""
+                                    if datas[0].lower()=="card":
+                                        print(datas[0] )
+                                        main_f(card=True)
+                                    else:
+                                        main_f(card=False)
+                                    break
+
+
                             else:
-                                main_f(card=False)
-                            break
 
+                                if digit[-1].isdigit():
+                                    rfid_tag += digit[-1]
+                else:
+                    print('RFID reader not found.')
+                    time.sleep(5)
 
-                    else:
+        thread1 = threading.Thread(target=antina)
+        thread2 = threading.Thread(target=card_reader)
 
-                        if digit[-1].isdigit():
-                            rfid_tag += digit[-1]
-        else:
-            print('RFID reader not found.')
-            time.sleep(5)
+        thread1.start()
+        thread2.start()
 
-thread1 = threading.Thread(target=antina)
-thread2 = threading.Thread(target=card_reader)
-
-thread1.start()
-thread2.start()
+        break
+    except:
+        print("Error in database")
+        continue
--- servo.py
+++ servo.py
@@ -5,8 +5,7 @@ import time
 # GPIO pin numbers
 LED_PIN = 25
 LED_RED_PIN = 17
-SERVO_PIN = 18
-servo = AngularServo(SERVO_PIN, min_pulse_width=0.0006, max_pulse_width=0.0023)
+
 
 
 
@@ -31,13 +30,18 @@ def turn_off_red():
     print("LED_RED_PIN turned off")
 
 def turn_on_led():
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
     # Set up LED pin as output
     GPIO.setup(LED_PIN, GPIO.OUT)
     GPIO.output(LED_PIN, GPIO.HIGH)
     print("LED turned on")
 
 def turn_off_led():
-    GPIO.setup(LED_PIN, GPIO.OUT)
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
     GPIO.output(LED_PIN, GPIO.LOW)
     print("LED turned off")
 
@@ -45,20 +49,28 @@ def cleanup():
     GPIO.cleanup()  # Reset GPIO pins
 
 def main_f(card):
+    GPIO.setmode(GPIO.BCM)
+
+    SERVO_PIN = 18
+    servo = AngularServo(SERVO_PIN, min_pulse_width=0.0006, max_pulse_width=0.0023)
+    servo.angle = 0
     try:
         # Turn on LED for 3 seconds
         turn_on_led()
         time.sleep(1)
         if card == False:
+            print("Servo opening")
             servo.angle = 90
             time.sleep(10)
             servo.angle = 0
-        time.sleep(2)
+            print("Servo Closing")
+        time.sleep(1)
+
 
         # Turn off LED
         turn_off_led()
 
+        cleanup()
+
     except KeyboardInterrupt:
         print("Script interrupted.")
-    finally:
-        cleanup()
